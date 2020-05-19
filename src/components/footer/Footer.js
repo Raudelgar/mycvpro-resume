6c3d2998fd1324e75d26895f5fb51b920780fab6
raudelgar <raudel24@gmail.com>
2020-05-19 11:12:45 -0400

    footer in dark theme

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import './footer.scss';
+import ThemeContext from '../../context/ThemeContext.js';
 
 export default function Footer() {
+	const { theme } = useContext(ThemeContext);
 	return (
 		<footer className='footer-container'>
 			<div className='footer-left'>
 				<div className='copyright'>
-					<span>Copyright &#9400; 2020 MyCvPro LLC. All rights reserved.</span>
+					<span className={`${theme}-text`}>
+						Copyright &#9400; 2020 MyCvPro LLC. All rights reserved.
+					</span>
 				</div>
 				<div className='footer-nav'>
-					<a href='#'>Privacy Policy</a>
-					<span className='tab'>|</span>
-					<a href='#'>About Us</a>
-					<span className='tab'>|</span>
-					<a href='#'>Contact Us</a>
-					<span className='tab'>|</span>
-					<a href='#'>Social Media</a>
+					<a href='#' className={`${theme}-footer-link`}>
+						Privacy Policy
+					</a>
+					<span className={`${theme}-tab ${theme}-text`}>|</span>
+					<a href='#' className={`${theme}-footer-link`}>
+						About Us
+					</a>
+					<span className={`${theme}-tab ${theme}-text`}>|</span>
+					<a href='#' className={`${theme}-footer-link`}>
+						Contact Us
+					</a>
+					<span className={`${theme}-tab ${theme}-text`}>|</span>
+					<a href='#' className={`${theme}-footer-link`}>
+						Social Media
+					</a>
 				</div>
 			</div>
 			<div className='region'>
-				<span className='lenguage'>en</span>
-				<span className='tab'>|</span>
-				<span className='country'>United State</span>
+				<span className={`${theme}-lenguage ${theme}-text`}>en</span>
+				<span className={`${theme}-tab ${theme}-text`}>|</span>
+				<span className={`${theme}-country ${theme}-text`}>United State</span>
 			</div>
 		</footer>
 	);
