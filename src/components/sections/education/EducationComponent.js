73cf28ca2af014c4eb7c2934e438f18a2792d432
raudelgar <raudel24@gmail.com>
2020-05-22 09:44:51 -0400

    education component completed

--- src/components/sections/education/EducationComponent.js
+++ src/components/sections/education/EducationComponent.js
@@ -3,16 +3,16 @@ import React, { useContext } from 'react';
 import { ThemeContext } from '../../../context/Context.js';
 import useEducationState from '../../../hooks/useEducationState.js';
 import SectionHeader from '../SectionHeader.js';
-// import SkillContent from './SkillContent.js';
+import EducationContent from './EducationContent.js';
 
 export default function EducationComponent() {
 	const { theme } = useContext(ThemeContext);
 	const { section, content } = useEducationState();
-	// console.log(section, content);
+
 	return (
 		<div className={`${theme}-grid-item`}>
 			<SectionHeader header={section} theme={theme} />
-			{JSON.stringify(content, null, 2)}
+			<EducationContent content={content} theme={theme} />
 		</div>
 	);
 }
