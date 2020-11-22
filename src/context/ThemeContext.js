2c22daf25a3a0aa901072418ed08d5391d351d05
raudelgar <raudel24@gmail.com>
2020-11-22 15:55:29 -0500

    setting theme on localstorage

--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,12 +1,27 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = (props) => {
-	const [theme, setTheme] = useState('dark');
+	const [theme, setTheme] = useState('');
 
-	const handleTheme = () =>
+	const handleTheme = () => {
 		setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
+	};
+
+	useEffect(() => {
+		const usrTheme = window.localStorage.getItem('cvcode_usr_th');
+		if (!usrTheme) {
+			window.localStorage.setItem('cvcode_usr_th', 'dark');
+			setTheme('dark');
+		} else {
+			setTheme(usrTheme);
+		}
+	}, []);
+
+	useEffect(() => {
+		window.localStorage.setItem('cvcode_usr_th', theme);
+	}, [theme]);
 
 	return (
 		<ThemeContext.Provider value={{ theme, handleTheme }}>
